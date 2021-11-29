7a7bd22b41f264a4f5ecc6d68059ecada988a5b4
Navneet <navn0306@gmail.com>
2021-11-29 16:25:25 +0530

    ADD: Added the program to generate the random coupon numbers

--- src/com/bridge/coupon/Coupon.java
+++ src/com/bridge/coupon/Coupon.java
@@ -2,6 +2,17 @@ package com.bridge.coupon;
 
 public class Coupon {
     public static void main(String[] args) {
-        System.out.println("\nWelcome, this program generates random coupon numbers");
+        char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789".toCharArray();
+        int max = 100000000;
+        int random = (int) (Math.random() * max);
+        StringBuffer sb = new StringBuffer();
+
+        while (random > 0) {
+            sb.append(chars[random % chars.length]);
+            random /= chars.length;
+        }
+
+        String couponCode = sb.toString();
+        System.out.println("Coupon Code: " + couponCode);
     }
 }
