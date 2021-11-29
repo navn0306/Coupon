822153f4e1f6e636704d79b8f02933a2036e42b8
Navneet <navn0306@gmail.com>
2021-11-29 16:23:26 +0530

    ADD: Added the welcome message for project

---
+++ src/com/bridge/coupon/Coupon.java
@@ -0,0 +1,7 @@
+package com.bridge.coupon;
+
+public class Coupon {
+    public static void main(String[] args) {
+        System.out.println("\nWelcome, this program generates random coupon numbers");
+    }
+}
